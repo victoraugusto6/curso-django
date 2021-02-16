449fbdc649820a546892a947004a3931bee5dc7d
Victor Augusto <victor.augusto60@gmail.com>
2021-02-15 21:27:13 -0300

    Corrigido collectfast
    close #35

--- pypro/settings.py
+++ pypro/settings.py
@@ -32,6 +32,9 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Application definition
 
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+COLLECTFAST_STRATEGY = "collectfast.strategies.boto3.Boto3Strategy"
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',

22d3608d8905d3d444d89314a7f3984477224e56
Victor Augusto <victor.augusto60@gmail.com>
2021-02-22 19:20:39 -0300

    Aplicado Bootstrap

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -1,7 +1,6 @@
-from django.http import HttpResponse
-
-
 # Create your views here.
+from django.shortcuts import render
+
 
 def home(request):
-    return HttpResponse('<html><body>Olá Django</body></html>', content_type='text/html')
+    return render(request, 'base/home.html')

c16a1f7ec0f0165f4516ea2648612b79804fc297
Victor Augusto <victor.augusto60@gmail.com>
2021-02-22 12:40:11 -0300

    Provocando erro forçado para Sentry detectar

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
+    raise ValueError()
     return HttpResponse('<html><body>Olá Django</body></html>', content_type='text/html')
--- pypro/settings.py
+++ pypro/settings.py
@@ -13,6 +13,9 @@ import os
 from functools import partial
 from pathlib import Path
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 import dj_database_url
 from decouple import config, Csv
@@ -171,3 +174,15 @@ if AWS_ACCESS_KEY_ID:  # pragma: no cover
 
     INSTALLED_APPS.append('s3_folder_storage')
     INSTALLED_APPS.append('storages')
+
+SENTRY_DSN = config('SENTRY_DSN', default=None)
+
+sentry_sdk.init(
+    dsn=SENTRY_DSN,
+    integrations=[DjangoIntegration()],
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)

5d400e256922770a1a434811613b229a68636874
Victor Augusto <victor.augusto60@gmail.com>
2021-02-24 12:59:57 -0300

    Implementado Contexto de Template

--- pypro/aperitivos/tests.py
+++ pypro/aperitivos/tests.py
@@ -14,7 +14,7 @@ def test_status_code(resp):
 
 
 def test_titulo_video(resp):
-    assert_contains(resp, '<h1>Vídeo Aperitivo: Motivação</h1>')
+    assert_contains(resp, '<h1 class="mt-4 mb-3">Vídeo Aperitivo: Motivação</h1>')
 
 
 def test_conteudo_video(resp):

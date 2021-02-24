1e2d16a7c85888d6514acc2f3a02ed8f995e9d55
Victor Augusto <victor.augusto60@gmail.com>
2021-02-24 14:47:10 -0300

    Criar página para segundo vídeo aperitivo

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    video = {'titulo': 'Vídeo Aperitivo: Motivação', 'vimeo_id': 508233665}
+    videos = {
+        'motivacao': {'titulo': 'Vídeo Aperitivo: Motivação', 'vimeo_id': 508233665},
+        'instalacao-windows': {'titulo': 'Instalando extensões do Gnome', 'vimeo_id': 516326786},
+    }
+    video = videos[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})

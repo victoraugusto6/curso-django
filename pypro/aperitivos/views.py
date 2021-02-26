826a6e75f275e4c5f110e062e9fb0e2c4f9fb1d3
Victor Augusto <victor.augusto60@gmail.com>
2021-02-26 20:23:27 -0300

    Instalado Docker no projeto
    close #98

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,20 +1,10 @@
 from django.shortcuts import render
-from django.urls import reverse
 
-
-class Video:
-    def __init__(self, slug, titulo, vimeo_id):
-        self.slug = slug
-        self.titulo = titulo
-        self.vimeo_id = vimeo_id
-
-    def get_absolute_url(self):
-        return reverse('aperitivos:video', args=(self.slug,))
-
+from pypro.aperitivos.models import Video
 
 videos = [
-    Video('motivacao', 'Vídeo Aperitivo: Motivação', 508233665),
-    Video('instalacao-windows', 'Instalando extensões do Gnome', 516326786),
+    Video(slug='motivacao', titulo='Vídeo Aperitivo: Motivação', vimeo_id='508233665'),
+    Video(slug='instalacao-windows', titulo='Instalando extensões do Gnome', vimeo_id='516326786'),
 ]
 
 videos_dct = {v.slug: v for v in videos}

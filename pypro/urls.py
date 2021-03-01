e412a074e04ddf2c169aa98cb70abd2d5dfefb12
Victor Augusto <victor.augusto60@gmail.com>
2021-03-01 14:22:39 -0300

    Consertando Links do Dropdown

--- pypro/urls.py
+++ pypro/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pypro.base.urls')),
     path('aperitivos/', include('pypro.aperitivos.urls')),
+    path('modulos/', include('pypro.modulos.urls')),
 ]
 
 if settings.DEBUG:  # pragma: no cover

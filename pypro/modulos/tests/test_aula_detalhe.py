2aa799a0230a0397b354ffb320c2add069a40c91
Victor Augusto <victor.augusto60@gmail.com>
2021-03-02 14:23:25 -0300

    Adição de Coluna em Tabela
    close #116

--- pypro/modulos/tests/test_aula_detalhe.py
+++ pypro/modulos/tests/test_aula_detalhe.py
@@ -24,3 +24,7 @@ def resp(client, aula):
 
 def test_titulo(resp, aula: Aula):
     assert_contains(resp, aula.titulo)
+
+
+def test_vimeo(resp, aula: Aula):
+    assert_contains(resp, f'src="https://player.vimeo.com/video/{aula.vimeo_id}"')

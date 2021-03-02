e69873cc6b6e428a9f4707723fbe562dd653aef4
Victor Augusto <victor.augusto60@gmail.com>
2021-03-02 16:01:48 -0300

    Objeto Prefetch
    close #118

--- pypro/modulos/facade.py
+++ pypro/modulos/facade.py
@@ -1,5 +1,7 @@
 from typing import List
 
+from django.db.models import Prefetch
+
 from pypro.modulos.models import Modulo, Aula
 
 
@@ -25,4 +27,6 @@ def encontrar_aula(slug):
 
 
 def listar_modulos_com_aulas():
-    return Modulo.objects.order_by('order').prefetch_related('aula_set').all()
+    aulas_ordenadas = Aula.objects.order_by('order')
+    return Modulo.objects.order_by('order').prefetch_related(
+        Prefetch('aula_set', queryset=aulas_ordenadas, to_attr='aulas')).all()

457bd3c2be7ee8f40afa1839cc8c191010c519c5
Victor Augusto <victor.augusto60@gmail.com>
2021-03-02 15:25:48 -0300

    Implementando um Breadcrumb

--- pypro/modulos/tests/test_aula_detalhe.py
+++ pypro/modulos/tests/test_aula_detalhe.py
@@ -28,3 +28,7 @@ def test_titulo(resp, aula: Aula):
 
 def test_vimeo(resp, aula: Aula):
     assert_contains(resp, f'src="https://player.vimeo.com/video/{aula.vimeo_id}"')
+
+
+def test_modulo_breadcrumb(resp, modulo: Aula):
+    assert_contains(resp, f'<li class="breadcrumb-item"><a href="{modulo.get_absolute_url()}">{modulo.titulo}</a></li>')

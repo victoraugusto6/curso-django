2cd79958610eaeb22eb361245066baa2c385ea55
Victor Augusto <victor.augusto60@gmail.com>
2021-03-04 15:40:09 -0300

    Teste com Usuário Logado

--- pypro/base/tests/test_login.py
+++ pypro/base/tests/test_login.py
@@ -2,7 +2,7 @@ import pytest
 from django.urls import reverse
 from model_mommy import mommy
 
-from pypro.django_assertions import assert_contains
+from pypro.django_assertions import assert_contains, assert_not_contains
 
 
 @pytest.fixture
@@ -45,3 +45,16 @@ def test_botao_entrar_disponivel(resp_home):
 
 def test_link_de_login_disponivel(resp_home):
     assert_contains(resp_home, reverse('login'))
+
+
+@pytest.fixture
+def resp_home_com_usuario_logado(client_com_usuario_logado, db):
+    return client_com_usuario_logado.get(reverse('base:home'))
+
+
+def test_botao_entrar_indisponivel(resp_home_com_usuario_logado):
+    assert_not_contains(resp_home_com_usuario_logado, 'Entrar')
+
+
+def test_link_de_login_indisponivel(resp_home_com_usuario_logado):
+    assert_not_contains(resp_home_com_usuario_logado, reverse('login'))

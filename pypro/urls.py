9f7d66bff295a26a350e2fc65000949e27892170
Victor Augusto <victor.augusto60@gmail.com>
2021-05-27 22:58:48 -0300

    Aplicado Django Debug Toolbar
    close #29

--- pypro/urls.py
+++ pypro/urls.py
@@ -14,11 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
+from django.conf import settings
 from pypro.base import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns.append(
+        path('__debug__/', include(debug_toolbar.urls))
+    )

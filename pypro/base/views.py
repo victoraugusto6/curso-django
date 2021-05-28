3e20dac07e98c29ca16efd4fde517d61705d5fb1
Victor Augusto <victor.augusto60@gmail.com>
2021-05-28 18:08:51 -0300

    Configurado arquivos estáticos localmente
    close #33

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('<html><body>Olá Django</body></html>', content_type='text/html')
+    return render(request, 'base/home.html')

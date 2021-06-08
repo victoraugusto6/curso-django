a489adcfcef1c4055b401a635be414ec4acbfbd4
Victor Augusto <victor.augusto60@gmail.com>
2021-06-08 15:36:27 -0300

    Publicando vídeo vimeo

--- pypro/urls.py
+++ pypro/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pypro.base.urls')),
+    path('aperitivos/', include('pypro.aperitivos.urls')),
 ]
 
 if settings.DEBUG:

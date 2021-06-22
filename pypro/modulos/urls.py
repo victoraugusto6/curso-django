51a371ffcd3f0f0e76a2372de65ebcccdb9e2e60
Victor Augusto <victor.augusto60@gmail.com>
2021-06-22 18:18:24 -0300

    Funcionalidade de Indice de Módulos

--- pypro/modulos/urls.py
+++ pypro/modulos/urls.py
@@ -5,4 +5,5 @@ app_name = 'modulos'
 urlpatterns = [
     path('<slug:slug>', views.detalhe, name='detalhe'),
     path('aulas/<slug:slug>', views.aula, name='aula'),
+    path('', views.indice, name='indice'),
 ]

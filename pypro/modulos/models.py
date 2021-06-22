d9e3cd33582dcceace55fb24946a10ea681a368c
Victor Augusto <victor.augusto60@gmail.com>
2021-06-22 12:59:35 -0300

    Migração de Dados

--- pypro/modulos/models.py
+++ pypro/modulos/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.urls import reverse
 from ordered_model.models import OrderedModel
 
 
 class Modulo(OrderedModel):
     titulo = models.CharField(max_length=64)
+    slug = models.SlugField(unique=True)
     publico = models.TextField()
     descricao = models.TextField()
 
@@ -12,3 +14,6 @@ class Modulo(OrderedModel):
 
     def __str__(self):
         return self.titulo
+
+    def get_absolute_url(self):
+        return reverse('modulos:detalhe', kwargs={'slug': self.slug})

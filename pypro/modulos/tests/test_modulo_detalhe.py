ad6d8647bd3ed1e7cec358eb714df134eaf91c97
Victor Augusto <victor.augusto60@gmail.com>
2021-06-22 13:16:22 -0300

    Construção da Página de Detalhes do Módulo

---
+++ pypro/modulos/tests/test_modulo_detalhe.py
@@ -0,0 +1,29 @@
+from django.urls.base import reverse
+import pytest
+from model_mommy import mommy
+
+from pypro.django_assertions import assert_contains
+from pypro.modulos.models import Modulo
+
+
+@pytest.fixture
+def modulo(db):
+    return mommy.make(Modulo)
+
+
+@pytest.fixture
+def resp(client, modulo):
+    resp = client.get(reverse('modulos:detalhe', kwargs={'slug': modulo.slug}))
+    return resp
+
+
+def test_titulo(resp, modulo: Modulo):
+    assert_contains(resp, modulo.titulo)
+
+
+def test_descricao(resp, modulo: Modulo):
+    assert_contains(resp, modulo.descricao)
+
+
+def test_publico(resp, modulo: Modulo):
+    assert_contains(resp, modulo.publico)

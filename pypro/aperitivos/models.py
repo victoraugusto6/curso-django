f3f96f09505599612004778906e0ddc5d529c890
Victor Augusto <victor.augusto60@gmail.com>
2021-06-22 00:37:04 -0300

    Final de Testes de Video e Criação de Migração

--- pypro/aperitivos/models.py
+++ pypro/aperitivos/models.py
@@ -6,6 +6,7 @@ class Video(models.Model):
     slug = models.CharField(max_length=32)
     titulo = models.CharField(max_length=32)
     vimeo_id = models.CharField(max_length=32)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def get_absolute_url(self):
         return reverse('aperitivos:video', args=(self.slug,))
--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,18 +2,12 @@ from django.shortcuts import render, get_object_or_404
 
 from pypro.aperitivos.models import Video
 
-videos = [
-    Video(slug='motivacao', titulo='Vídeo Aperitivo: Motivação', vimeo_id='516326786'),
-    Video(slug='instalacao-windows', titulo='Dual boot popOs - Windows', vimeo_id='508233665'),
-]
 
-videos_dct = {v.slug: v for v in videos}
+def indice(request):
+    videos = Video.objects.order_by('created_at').all()
+    return render(request, 'aperitivos/indice.html', context={'videos': videos})
 
 
 def video(request, slug):
     video = get_object_or_404(Video, slug=slug)
     return render(request, 'aperitivos/video.html', context={'video': video})
-
-
-def indice(request):
-    return render(request, 'aperitivos/indice.html', context={'videos': videos})

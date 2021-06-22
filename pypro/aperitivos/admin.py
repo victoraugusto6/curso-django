4f545f8f697e8609f6127e5e799ef81a220c6821
Victor Augusto <victor.augusto60@gmail.com>
2021-06-22 00:50:37 -0300

    Introdução ao Admin do Django

---
+++ pypro/aperitivos/admin.py
@@ -0,0 +1,10 @@
+from django.contrib.admin import ModelAdmin, register
+
+from pypro.aperitivos.models import Video
+
+
+@register(Video)
+class VideoAdmin(ModelAdmin):
+    list_display = ('titulo', 'slug', 'created_at', 'vimeo_id')
+    ordering = ('created_at',)
+    prepopulated_fields = {'slug': ('titulo',)}
--- pypro/aperitivos/models.py
+++ pypro/aperitivos/models.py
@@ -3,10 +3,13 @@ from django.urls import reverse
 
 
 class Video(models.Model):
-    slug = models.CharField(max_length=32)
     titulo = models.CharField(max_length=32)
+    slug = models.SlugField(max_length=32)
     vimeo_id = models.CharField(max_length=32)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def get_absolute_url(self):
         return reverse('aperitivos:video', args=(self.slug,))
+
+    def __str__(self):
+        return self.titulo

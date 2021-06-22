4359f944cd4ea34ca341d920a0a00ad4652a4a69
Victor Augusto <victor.augusto60@gmail.com>
2021-06-22 11:21:18 -0300

    Requisitos da App Módulos de Aulas

---
+++ pypro/modulos/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ModulosConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'pypro.modulos'
--- pypro/settings.py
+++ pypro/settings.py
@@ -38,6 +38,7 @@ AUTH_USER_MODEL = 'base.User'
 INSTALLED_APPS = [
     'pypro.base',
     'pypro.aperitivos',
+    'pypro.modulos',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

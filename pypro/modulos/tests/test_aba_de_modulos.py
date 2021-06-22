b2b52a42c872e0db8b8ed07a96fc233b2c74a974
Victor Augusto <victor.augusto60@gmail.com>
2021-06-22 11:27:58 -0300

    Teste de Títulos de Aba Módulos

---
+++ pypro/modulos/tests/test_aba_de_modulos.py
@@ -0,0 +1,22 @@
+from django.urls.base import reverse
+import pytest
+from model_mommy import mommy
+
+from pypro.django_assertions import assert_contains
+from pypro.modulos.models import Modulo
+
+
+@pytest.fixture
+def modulos(db):
+    return mommy.make(Modulo, 2)
+
+
+@pytest.fixture
+def resp(client, modulos):
+    resp = client.get(reverse('base:home'))
+    return resp
+
+
+def test_titulos_dos_modulos(resp, modulos):
+    for modulo in modulos:
+        assert_contains(resp, modulo.titulo)

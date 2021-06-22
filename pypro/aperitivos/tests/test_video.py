12c124e4cdc0b78f255dbc5eb0a50776bc8f8903
Victor Augusto <victor.augusto60@gmail.com>
2021-06-22 00:12:23 -0300

    Criação Automágica de Propriedades de Modelo

--- pypro/aperitivos/tests/test_video.py
+++ pypro/aperitivos/tests/test_video.py
@@ -1,5 +1,6 @@
 import pytest
 from django.urls import reverse
+from model_mommy import mommy
 
 from pypro.aperitivos.models import Video
 from pypro.django_assertions import assert_contains
@@ -7,13 +8,7 @@ from pypro.django_assertions import assert_contains
 
 @pytest.fixture
 def video(db):
-    v = Video(
-        slug='motivacao',
-        titulo='Vídeo Aperitivo: Motivação',
-        vimeo_id='516326786'
-    )
-    v.save()
-    return v
+    return mommy.make(Video)
 
 
 @pytest.fixture

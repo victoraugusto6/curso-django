494f5e7ace5716a2e5a050fd0503e3ea58e6de05
Victor Augusto <victor.augusto60@gmail.com>
2021-06-21 23:54:42 -0300

    Executar Testes Com e Sem Migrações

--- pypro/aperitivos/tests/test_video.py
+++ pypro/aperitivos/tests/test_video.py
@@ -5,7 +5,7 @@ from pypro.django_assertions import assert_contains
 
 
 @pytest.fixture
-def resp(client):
+def resp(client, db):
     return client.get(reverse('aperitivos:video', args=('motivacao',)))
 
 

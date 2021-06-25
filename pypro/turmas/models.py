d222fb669b6acefd9bb72d84b287de9f8abbe03e
Victor Augusto <victor.augusto60@gmail.com>
2021-06-24 22:57:30 -0300

    Campo ManyToManyField

--- pypro/turmas/models.py
+++ pypro/turmas/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 
@@ -6,3 +7,7 @@ class Turma(models.Model):
     slug = models.SlugField(max_length=64)
     inicio = models.DateField()
     fim = models.DateField()
+    matriculas = models.ManyToManyField(get_user_model())
+
+    def __str__(self):
+        return self.nome

4cd5f8ec0739d1fb54f6d3a90b57c7c2b74410a1
Victor Augusto <victor.augusto60@gmail.com>
2021-06-24 23:12:15 -0300

    Criação de Tabela Intermediária

--- pypro/turmas/models.py
+++ pypro/turmas/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 
@@ -6,6 +7,13 @@ class Turma(models.Model):
     slug = models.SlugField(max_length=64)
     inicio = models.DateField()
     fim = models.DateField()
+    alunos = models.ManyToManyField(get_user_model(), through='Matricula')
 
     def __str__(self):
         return self.nome
+
+
+class Matricula(models.Model):
+    data = models.DateTimeField(auto_now_add=True)
+    usuario = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    turma = models.ForeignKey(Turma, on_delete=models.CASCADE)

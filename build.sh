b280cf63debee6b2ade98db27bea1aced44a6089
Victor Augusto <victor.soares@irricontrol.com.br>
2022-08-26 14:33:00 -0300

    remove createsuperuser in render build

--- build.sh
+++ build.sh
@@ -4,10 +4,9 @@ set -o errexit
 
 pip install -r requirements.txt
 
-export DJANGO_SUPERUSER_EMAIL=victor.augusto.soliveira@gmail.com
-export DJANGO_SUPERUSER_PASSWORD=deployinrender
-
-python manage.py createsuperuser --no-input
+#export DJANGO_SUPERUSER_EMAIL=victor.augusto.soliveira@gmail.com
+#export DJANGO_SUPERUSER_PASSWORD=deployinrender
+#python manage.py createsuperuser --no-input
 
 python manage.py collectstatic --no-input
 python manage.py migrate

ce8848025ac5e8b915d1c58f5dc9d860a4ee213a
Victor Augusto <victor.soares@irricontrol.com.br>
2022-12-16 21:46:42 -0300

    files from fly.io

--- pypro/settings.py
+++ pypro/settings.py
@@ -29,8 +29,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-# ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+CSRF_TRUSTED_ORIGINS = ['https://curso-django-devpro.fly.dev']
 
 AUTH_USER_MODEL = 'base.User'
 
